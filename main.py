4ad2cd560bbb17e23c419575401c08456bc754ba
mrq630084-tech <mrq630084@gmail.com>
2026-03-05 05:41:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
+import telebot
+import os
 from faster_whisper import WhisperModel
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
 
 model = WhisperModel("small", compute_type="int8")
 
-@bot.message_handler(content_types=['audio','voice','document'])
-def handle_audio(message):
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Video or audio send me. I will create SRT.")
+
+@bot.message_handler(content_types=['audio','voice','video','document'])
+def handle_file(message):
 
     file_info = bot.get_file(message.audio.file_id if message.audio else message.document.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
+    downloaded = bot.download_file(file_info.file_path)
 
-    with open("audio.mp3", "wb") as f:
-        f.write(downloaded_file)
+    with open("input.mp3","wb") as f:
+        f.write(downloaded)
 
-    result = model.transcribe("audio.mp3")
+    segments, info = model.transcribe("input.mp3")
 
-    with open("subtitles.srt", "w", encoding="utf-8") as f:
-        for i, seg in enumerate(result["segments"], start=1):
-            start = seg["start"]
-            end = seg["end"]
-            text = seg["text"]
+    srt = ""
+    i = 1
 
-            f.write(f"{i}\n")
-            f.write(f"{start} --> {end}\n")
-            f.write(f"{text}\n\n")
+    for segment in segments:
+        start = segment.start
+        end = segment.end
+        text = segment.text
 
-    bot.send_document(message.chat.id, open("subtitles.srt","rb"))
+        srt += f"{i}\n{start:.2f} --> {end:.2f}\n{text}\n\n"
+        i += 1
+
+    with open("output.srt","w",encoding="utf-8") as f:
+        f.write(srt)
+
+    bot.send_document(message.chat.id, open("output.srt","rb"))
+
+bot.infinity_polling()
