5a017bf6fa9e76f534e52c8a1b318bd231f13384
mrq630084-tech <mrq630084@gmail.com>
2026-03-05 02:53:43 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import threading
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "SRT Bot Running"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = threading.Thread(target=run)
+    t.start()
+
+keep_alive()
