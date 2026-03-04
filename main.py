dbcc7833271a002330f89499d010824d637af403
mrq630084-tech <mrq630084@gmail.com>
2026-03-05 03:50:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-from flask import Flask
-import threading
+import os
+from flask import Flask, request
+import telebot
 
-app = Flask('')
+TOKEN = "8514932008:AAGX3GyNd-9t8GBEYHm7JYfEZlztxAJvr4A"
 
-@app.route('/')
+bot = telebot.TeleBot(TOKEN)
+app = Flask(__name__)
+
+# start command
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "SRT Bot Ready. Send me MP3.")
+
+# receive audio
+@bot.message_handler(content_types=['audio','voice','document'])
+def handle_audio(message):
+    bot.reply_to(message, "Processing...")
+
+# webhook route
+@app.route(f"/{TOKEN}", methods=["POST"])
+def webhook():
+    json_str = request.get_data().decode("UTF-8")
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "ok", 200
+
+# home route
+@app.route("/")
 def home():
     return "SRT Bot Running"
 
-def run():
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=f"https://waigyi-srt.up.railway.app/{TOKEN}")
 
-def keep_alive():
-    t = threading.Thread(target=run)
-    t.start()
-
-keep_alive()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
